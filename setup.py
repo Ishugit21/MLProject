2f8192db5efaf865b8a1f738e8fc3d5ad7b16ec7
Ishugit21 <iswaryasai.2147@gmail.com>
2026-03-16 16:36:38 -0400

    setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+from typing import List
+
+
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.strip() for req in requirements]
+
+        if "-e ." in requirements:
+            requirements.remove("-e .")
+
+    return requirements
+
+setup(
+    name = 'MLPROJECTS',
+    version = '0.0.1',
+    author = 'Revathy Iswarya Sekaran',
+    author_email='iswaryasai.2147@gmail.com',
+    packages = find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
